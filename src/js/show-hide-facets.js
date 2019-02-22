1658604eb7c37aa2bc9aa423e2569721bf05c3b6
Bas Doppen <basdoppen@gmail.com>
2019-02-22 15:39:12 +0100

    Responsive facets

---
+++ src/js/show-hide-facets.js
@@ -0,0 +1,24 @@
+var facetsOpen = false;
+
+var clFacets = document.querySelectorAll("#showFacets");
+  for (i = 0; i < clFacets.length; i++) {
+
+    clFacets[i].addEventListener('click', function() {
+      if (facetsOpen ==true) {
+        facetsOpen = false;
+      } else {
+        facetsOpen = true;
+      }
+      handleMenu();
+
+    });
+}
+
+function handleMenu() {
+  if (facetsOpen == true) {
+    document.getElementById("hcLayoutFacetsToggel").style.display = "flex";
+  }
+  if (facetsOpen == false) {
+    document.getElementById("hcLayoutFacetsToggel").style.display = "none";
+  }
+}
