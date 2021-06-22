247c75e2f83232da26e31bea49fe5a224db4602a
BD <basdoppen@gmail.com>
2021-06-22 15:04:50 +0200

    ga

--- _dist/js/utilGA1.js
+++ _dist/js/utilGA1.js
@@ -64,7 +64,7 @@ function seletedLabel(contentId) {
 
 firstTabVisable();
 
-let splitInterfaceVal = true;
+let splitInterfaceVal = false;
 
 document.getElementById('hcCodeEditor').style.display= 'none';
 document.getElementById('qb').classList.add("bgColorBrand1");
@@ -107,25 +107,46 @@ console.log('v=',splitInterfaceVal);
 
 
 
-
+//hcGaPanelRight
 function splitInterface(splitInterfaceVal) {
   if (splitInterfaceVal) {
+    // column
     document.getElementById('splitGa').classList.add("gaSplitCol");
+    document.getElementById('b_splitToggle').setAttribute("src", "images/custom/ga-agent/b_toggle_Row.png");
+    document.getElementById('hcGaPanelRight').style.maxWidth='100%';
   } else {
+    // rows
     document.getElementById('splitGa').classList.remove("gaSplitCol");
+    document.getElementById('b_splitToggle').setAttribute("src", "images/custom/ga-agent/b_toggle_Col.png");
+    document.getElementById('hcGaPanelRight').style.maxWidth='50vw';
+    if (x.matches) { // If media query matches
+      document.getElementById('hcGaPanelRight').style.maxWidth='100%';
+    }
   }
 }
 
 
 
 function myFunction(x) {
-  // if (x.matches) { // If media query matches
-  //   document.body.style.backgroundColor = "yellow";
-  // } else {
-  //   document.body.style.backgroundColor = "pink";
-  // }
+  if (x.matches) { // If media query matches
+    document.getElementById('b_splitToggle').style.display= 'none';
+
+  } else {
+    document.getElementById('b_splitToggle').style.display= 'flex';
+  }
 }
 
-var x = window.matchMedia("(max-width: 1000px)")
+var x = window.matchMedia("(max-width: 1300px)")
 myFunction(x) // Call listener function at run time
-//x.addListener(myFunction) // Attach listener function on state changes
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+function closeMsg() {
+  document.getElementById('msg01').style.display= 'none';
+}
+
+let timer;
+removeMsg();
+function removeMsg() {
+  timer = setTimeout(closeMsg, 10000);
+}
