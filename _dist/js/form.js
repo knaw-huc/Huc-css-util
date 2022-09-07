fd0e73877fe0e0b73b7249112c2782d67415c080
BasD <basdoppen@gmail.com>
2022-09-07 10:36:45 +0200

    button test

--- _dist/js/form.js
+++
@@ -1,8 +0,0 @@
-var focusOnFormItem = document.querySelectorAll(".hcpr");
-for (i = 0; i < focusOnFormItem.length; i++) {
-  //alert('open');
-  focusOnFormItem[i].addEventListener('focus', function() {
-    document.querySelector('.hcProvenanceComp').style.display = "flex";
-
-  });
-}
